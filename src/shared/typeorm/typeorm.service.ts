445bb4c65d56df4af82d41526109a19f846ec361
Andres Lopez <am.lopezr1@uniandes.edu.co>
2023-02-04 02:15:37 -0500

    actualizando configuracion de bd

--- src/shared/typeorm/typeorm.service.ts
+++ src/shared/typeorm/typeorm.service.ts
@@ -19,6 +19,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       migrations: ['dist/migrations/*.{ts,js}'],
       migrationsTableName: 'typeorm_migrations',
       logger: 'file',
+      schema: this.config.get<string>('DB_SCHEMA'),
       synchronize: true, // never use TRUE in production!
     };
   }
